b0f04930350ce2aa5ac8d6b30a26030a3868adc3
priegelsdev <priegelsdev@gmail.com>
2023-01-18 16:45:41 +0100

    initialize react app with vite and data file

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import data from './data'
+
+function App() {
+  const travelData = data.map(item => item)
+}
+
+export default App
